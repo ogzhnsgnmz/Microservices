e2df034ca40f2abfebe45e7dc53a1a14cc74f4d8
oguzh <oguzh@DESKTOP-E9MDQSC>
2023-08-12 05:11:44 +0300

    Add project files.

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Dtos/CategoryDto.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Catalog.Dtos
+{
+    public class CategoryDto
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
